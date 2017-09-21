72a06b4e55e544a26ff524892c4cbde642d507c8
william-shih <r97942115@gmail.com>
2017-09-21 15:41:07 +0800

    Use default name of server and output folder

--- ubuntu_12_04/Images_Download_Wizard_install.sh
+++ ubuntu_12_04/Images_Download_Wizard_install.sh
@@ -1,62 +1,32 @@
 #! /bin/bash
 
-# program name
-PROG=Images_Download_Wizard
-
 # username
 unset usr_name
 usr_name="$(whoami)"
 echo "username: $usr_name"
 
 # wizard root
-WROOT=/home/$usr_name/$PROG
-GET_IMG_CONFIG=$WROOT/.get_img_config
 CUR=`pwd`
+GET_IMG_CONFIG=$CUR/../.get_img_config
 
 # environment setting
 clear
-echo "Installing needed packages: Do not skip at first time!!"
-read -p "Skip?(y/n)" check
-if [ "$check" == "n" ]; then
-	echo "Installing packages ..."
-	sudo apt-get install cifs-utils nfs-common
-fi
+echo "Installing packages ..."
+sudo apt-get install cifs-utils nfs-common
 
 # mount remote server
+clear
 unset mount1 mount2
-check="n"
-if [ -f $GET_IMG_CONFIG ]; then
-	echo "Mount remote server: configure file already exist"
-	read -p "Skip?(y/n)" check
-fi
-yn=$check
-while [ "$yn" == "n" ]; do
-	clear
-	echo "We will mount remote server (Prj/Prj2) to your server (\$mount1/\$mount2)."
-	read -p "Enter dirname (\$mount1) -> Prj: " mount1
-	read -p "Enter dirname (\$mount2) -> Prj2: " mount2
-	echo "dir path: $WROOT/$mount1"
-	echo "          $WROOT/$mount2"
-	read -p "OK?(y/n)" yn
-	yn=$(echo $yn | perl -ne 'print lc')
-	echo $yn
-done
-if [ "$check" == "n" ]; then
-	clear
-	echo "Creating $mount1 $mount2 ..."
-	mkdir -p $WROOT/$mount1
-	mkdir -p $WROOT/$mount2
-fi
+mount1=3F_server1
+mount2=3F_server2
+echo "Creating $mount1 $mount2 ..."
+mkdir -p $CUR/../$mount1
+mkdir -p $CUR/../$mount2
 
 # set mount script
-unset fname
-unset OA_name
-check="n"
-if [ -f $GET_IMG_CONFIG ]; then
-	echo "Create mount script: configure file already exist"
-	read -p "Skip?(y/n)" check
-fi
-yn=$check
+unset FILENAME
+FILENAME=$CUR/../mount.sh
+yn="n"
 while [ "$yn" == "n" ]; do
 	clear
 	unset OA_name OA_pwd fname
@@ -64,68 +34,42 @@ while [ "$yn" == "n" ]; do
 	echo -n "Enter your OA password: "
 	read -s OA_pwd
 	echo
-	read -p "Enter filename of mount script: " fname
-	echo
 	echo "OA username=$OA_name"
 	echo "OA password=$OA_pwd"
-	echo "file path: $WROOT/$fname"
+	echo "file path: $FILENAME"
 	read -p "OK?(y/n)" yn
 	yn=$(echo $yn | perl -ne 'print lc')
 done
 
-if [ "$check" == "n" ]; then
-	clear
-	FILENAME=$WROOT/$fname
-	UMOUNT=$WROOT/umount.sh
-	echo "Creating $FILENAME ..."
-	touch $FILENAME
-	echo "sudo mount -t cifs //10.72.71.208/Prj/ $WROOT/$mount1 -o username=$OA_name,password=$OA_pwd" > $FILENAME
-	echo "sudo mount -t cifs //10.72.71.208/Prj2/ $WROOT/$mount2 -o username=$OA_name,password=$OA_pwd" >> $FILENAME
-	chmod 755 $WROOT/$fname
-	touch $UMOUNT
-	echo "sudo umount $mount1 $mount2" > $UMOUNT
-	echo "rm -rf $mount1 $mount2" >> $UMOUNT
-	chmod 755 $UMOUNT
-fi
+clear
+echo "Creating $FILENAME ..."
+touch $FILENAME
+echo "sudo mount -t cifs //10.72.71.208/Prj/ $CUR/../$mount1 -o username=$OA_name,password=$OA_pwd" > $FILENAME
+echo "sudo mount -t cifs //10.72.71.208/Prj2/ $CUR/../$mount2 -o username=$OA_name,password=$OA_pwd" >> $FILENAME
+chmod 755 $FILENAME
+
+# Create unmount script
+UMOUNT=$CUR/../umount.sh
+touch $UMOUNT
+echo "sudo umount $mount1 $mount2" > $UMOUNT
+echo "rm -rf $mount1 $mount2" >> $UMOUNT
+chmod 755 $UMOUNT
 
 # Create destination folder
 unset img_dir
-
-check="n"
-if [ -f $GET_IMG_CONFIG ]; then
-	clear
-	echo "Create destination folder: configure file already exist"
-	read -p "Skip?(y/n)" check
-fi
-yn=$check
-while [ "$yn" == "n" ]; do
-	clear
-	echo "We will copy img from server to \$dir."
-	read -p "Enter \$dir name: " img_dir
-	echo "dir path: $WROOT/$img_dir"
-	read -p "OK?(y/n)" yn
-	yn=$(echo $yn | perl -ne 'print lc')
-done
-
-if [ $check == "n" ]; then
-	clear
-	echo "Creating $WROOT/$img_dir ..."
-	mkdir -p $WROOT/$img_dir
-fi
+img_dir=Images
 
 clear
-width=6
-read -p "The default width is $width. Is it OK?(y/n)" yn
-while [ "$yn" == "n" ]; do
-	read -p "Set width as : " width
-	echo "width of array is $width now"
-	read -p "OK?(y/n)" yn
-	yn=$(echo $yn | perl -ne 'print lc')
-done
+echo "Creating $CUR/../$img_dir ..."
+mkdir -p $CUR/../$img_dir
+
+clear
+echo "The default width is 6"
+read -p "Reset width as : " width
 
 echo "Creating .get_img_config ..."
 
-
+yn="y"
 if [ -f $GET_IMG_CONFIG ]; then
 	clear
 	read -p "configure file exist, overwrite?(y/n)" yn
@@ -135,7 +79,7 @@ if [ "$yn" == "y" ]; then
 	echo "dir_name = $img_dir" > $GET_IMG_CONFIG
 	echo "remote_mount1 = \"$mount1\"" >> $GET_IMG_CONFIG
 	echo "remote_mount2 = \"$mount2\"" >> $GET_IMG_CONFIG
-	echo "script = \"$fname\"" >> $GET_IMG_CONFIG
+	echo "script = \"mount.sh\"" >> $GET_IMG_CONFIG
 	echo "width = $width" >> $GET_IMG_CONFIG
 fi
 
@@ -146,7 +90,7 @@ sudo $FILENAME
 
 # create symbolic link
 clear
-echo "Create symbolic link at $WROOT/get_official_image.pl ..."
+echo "Create symbolic link at $CUR/../get_official_image.pl ..."
 ln -s $CUR/Images_Download_Wizard.pl $CUR/../get_official_image.pl
 echo "Done. You can enjoy your download now!"
 echo "Try ./Download_Image_Wizard.pl"
